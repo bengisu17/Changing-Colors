3450f2bc8a6bf06b2b73cd7f178786d627c9ca32
bengisu17 <bengisuyavuz17@gmail.com>
2023-05-23 14:32:52 +0300

    blue added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var index = 0;
 
 function changeColors(){
-    var colors = ["red", "orange", "yellow", "green", "purple", "pink"];
+    var colors = ["red", "orange", "yellow", "green", "purple", "pink", "blue"];
 
     document.getElementsByTagName("body")[0].style.background = colors[index++];
 
